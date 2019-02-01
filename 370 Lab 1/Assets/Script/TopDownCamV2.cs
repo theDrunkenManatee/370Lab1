f7ac45e605c13511ece3c101e3667eeabf92a5c4
theDrunkenManatee <samorege@hendrix.edu>
2019-01-31 20:50:48 -0600

    Control

---
+++ 370 Lab 1/Assets/Script/TopDownCamV2.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownCamV2 : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 distance;
+    private float smooth = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        distance = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + distance;
+    }
+}
